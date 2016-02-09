ba1f466190de30c3edd1b31b9955fccbc96fd610
pascalallen <thomaspascalallen@yahoo.com>
2016-02-09 17:11:51 -0600

    Started working on advent of code day2.

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,133 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/day1', function()
+{
+	return View::make('day1');
+});
+
+Route::get('/day2', function()
+{
+	return View::make('day2');
+});
+
+Route::get('/day3', function()
+{
+	return View::make('day3');
+});
+
+Route::get('/day3', function()
+{
+	return View::make('day3');
+});
+
+Route::get('/day4', function()
+{
+	return View::make('day4');
+});
+
+Route::get('/day5', function()
+{
+	return View::make('day5');
+});
+
+Route::get('/day6', function()
+{
+	return View::make('day6');
+});
+
+Route::get('/day7', function()
+{
+	return View::make('day7');
+});
+
+Route::get('/day8', function()
+{
+	return View::make('day8');
+});
+
+Route::get('/day9', function()
+{
+	return View::make('day9');
+});
+
+Route::get('/day10', function()
+{
+	return View::make('day10');
+});
+
+Route::get('/day11', function()
+{
+	return View::make('day11');
+});
+
+Route::get('/day12', function()
+{
+	return View::make('day12');
+});
+
+Route::get('/day13', function()
+{
+	return View::make('day13');
+});
+
+Route::get('/day14', function()
+{
+	return View::make('day14');
+});
+
+Route::get('/day15', function()
+{
+	return View::make('day15');
+});
+
+Route::get('/day16', function()
+{
+	return View::make('day16');
+});
+
+Route::get('/day17', function()
+{
+	return View::make('day17');
+});
+
+Route::get('/day18', function()
+{
+	return View::make('day18');
+});
+
+Route::get('/day19', function()
+{
+	return View::make('day19');
+});
+
+Route::get('/day20', function()
+{
+	return View::make('day20');
+});
+
+Route::get('/day21', function()
+{
+	return View::make('day21');
+});
+
+Route::get('/day22', function()
+{
+	return View::make('day22');
+});
+
+Route::get('/day23', function()
+{
+	return View::make('day23');
+});
+
+Route::get('/day24', function()
+{
+	return View::make('day24');
+});
+
+Route::get('/day25', function()
+{
+	return View::make('day25');
+});
